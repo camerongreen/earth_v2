6f330dd9228401c50592af3f0c74d4bd87c272bd
Cameron Green <i@camerongreen.org>
2020-09-07 21:24:34 +1000

    Fixing lame header clicking thing

--- js/page.js
+++ js/page.js
@@ -1,31 +1,38 @@
 (function ($) {
-    $(document).ready(function () {
-        $("#banner").click(function () {
-            var path = window.location.pathname;
-            var parts = path.split("/");
-            var doc_location = "/" + parts[1];
-            document.location = doc_location;
-        });
-
-        if ($("#toTop")) {
-            $("#toTop a").click(function (e) {
-                window.scrollTo(0, 0);
-                e.preventDefault();
-                return false;
-            });
-            $(window).scroll(function () {
-                if ($(this).scrollTop() > $("#banner").height()) {
-                    $('#toTop:hidden').stop(true, true).fadeIn();
-                } else {
-                    $('#toTop').stop(true, true).fadeOut();
-                }
-            });
+  $(document).ready(() => {
+    $('#banner').click(() => {
+      var path = window.location.pathname;
+      var parts = path.split('/');
+      var docLocation = [];
+      for (let part of parts) {
+        docLocation.push(part);
+        if (['e', 'a', 'r', 't', 't', 'about'].indexOf(part) !== -1) {
+          break;
         }
-        $('#responsive-menu > li > a').click(function (e) {
-            $("#block-system-main-menu").toggle();
-            $("#block-system-main-menu ul.menu li.last").removeClass("expanded");
-            e.preventDefault();
-            return false;
-        });
+      }
+      document.location = docLocation.join('/');
     });
+
+    if ($('#toTop')) {
+      $('#toTop a').click(function (e) {
+        window.scrollTo(0, 0);
+        e.preventDefault();
+        return false;
+      });
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > $('#banner').height()) {
+          $('#toTop:hidden').stop(true, true).fadeIn();
+        }
+        else {
+          $('#toTop').stop(true, true).fadeOut();
+        }
+      });
+    }
+    $('#responsive-menu > li > a').click(function (e) {
+      $('#block-system-main-menu').toggle();
+      $('#block-system-main-menu ul.menu li.last').removeClass('expanded');
+      e.preventDefault();
+      return false;
+    });
+  });
 }(jQuery));
